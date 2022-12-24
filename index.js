59c0c47127574eba897ea06931e05726b5b205ce
maria-lemes <madulemes10@gmail.com>
2022-12-24 16:09:29 -0300

    3.4

--- index.js
+++ index.js
@@ -47,12 +47,11 @@ app.get('/api/persons/:id', (request, response) => {
       }
   })
 
-// app.delete('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     notes = notes.filter(note => note.id !== id)
-  
-//     response.status(204).end()
-// })
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+    response.status(204).end()
+  })
 
 // const generateId = () => 
 //     const maxId = notes.length > 0
