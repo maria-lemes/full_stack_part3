d062b23e7b6d70dd33a2e6a85d013476e5325d88
maria-lemes <madulemes10@gmail.com>
2022-12-25 23:02:51 -0300

    3.18

--- index.js
+++ index.js
@@ -32,30 +32,6 @@ app.use(errorHandler)
 
 
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
@@ -64,8 +40,11 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
   let currentDate = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+  Person.count({}).then(total =>
+    response.send(`<p>Phonebook has info for ${total} people</p>
   <p>${currentDate}</p>`)
+  )
+  
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
